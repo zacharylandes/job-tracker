6466a9a6c5a69a40f677009c53c8454e8fadfaed
Anna Dolan <anna.dolan@gmail.com>
2016-10-20 13:12:44 -0600

    Initial commit

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,54 @@
+class CompaniesController < ApplicationController
+  def index
+    @companies = Company.all
+  end
+
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new(company_params)
+    if @company.save
+      flash[:success] = "#{@company.name} added!"
+      redirect_to company_path(@company)
+    else
+      render :new
+    end
+  end
+
+  def show
+    company = Company.find(params[:id])
+    redirect_to company_jobs_path(company)
+  end
+
+  def edit
+    @company = Company.find(params[:id])
+  end
+
+  def update
+    @company = Company.find(params[:id])
+    @company.update(company_params)
+    if @company.save
+      flash[:success] = "#{@company.name} updated!"
+      redirect_to company_path(@company)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    company = Company.find(params[:id])
+    company.delete
+
+    flash[:success] = "#{company.name} was successfully deleted!"
+    redirect_to companies_path
+  end
+
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :city)
+  end
+end
